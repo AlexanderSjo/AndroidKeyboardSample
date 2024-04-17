eef8abac878d8c883461da88ceb8384bec6e9e04
Alexander Sjöman <alexander.sjoman@spminstrument.se>
2024-04-17 09:36:56 +0200

    Added

---
+++ AndroidKeyboardSample/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AndroidKeyboardSample.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ AndroidKeyboardSample/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AndroidKeyboardSample.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
